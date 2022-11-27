ce7a61eb7077ab983b6cb75990706cb853be1eac
haleychoi <chldudgus617@gmail.com>
2022-11-27 21:49:49 +0900

    차량 mbti 로직 구현

--- src/App.tsx
+++ src/App.tsx
@@ -1,25 +1,20 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import Greeting from './components/Starting';
+import TestContents from './components/TestContents';
+import Result from './components/Result';
+import Loading from './components/Loading';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<Greeting />} />
+        <Route path="/test" element={<TestContents />} />
+        <Route path="/result/:car" element={<Result />} />
+        <Route path="/loading" element={<Loading />} />
+      </Routes>
+    </BrowserRouter>
   );
 }
 
